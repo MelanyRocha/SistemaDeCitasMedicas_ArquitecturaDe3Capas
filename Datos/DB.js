43fdfbed7471aee26ba4a8c070652e265b4770ce
MelanyRocha <81879832+MelanyRocha@users.noreply.github.com>
2025-05-30 21:01:45 -0400

    Update DB.js
    Mi gato (Jhany) pisó el teclado xd

--- Datos/DB.js
+++ Datos/DB.js
@@ -3,3 +3,4 @@ export class DB {
         console.log(`Cita guardada en base de datos para ${cita.paciente} con ${cita.doctor} el ${cita.fecha}`);
     }
 }
+//assssssssssssssssssssssssssssssssssssssssssssssssssx
