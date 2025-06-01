fc1bd247639b828a8da002a39f9d22e3f1354103
MelanyRocha <81879832+MelanyRocha@users.noreply.github.com>
2025-06-01 19:22:35 -0400

    Create Main.js

---
+++ Presentacion/Main.js
@@ -0,0 +1,20 @@
+import { CitaFactory } from "../Negocio/CitaFactory.js";
+import { ServicioConRecordatorio } from "../Negocio/ServicioConRecordatorio.js";
+import { ServicioBasico } from "../Negocio/ServicioBasico.js";
+import { Subject } from "../Negocio/Subject.js";
+import { EmailObserver } from "../Negocio/EmailObserver.js";
+import { SMSObserver } from "../Negocio/SMSObserver.js";
+import { DB } from "../Datos/DB.js";
+
+const factory = new CitaFactory();
+const cita = factory.crearCita("Melany", "Dr. Juan Pérez", "2025-05-12");
+
+const servicio = new ServicioConRecordatorio(new ServicioBasico());
+servicio.agendar(cita);
+
+const notificador = new Subject();
+notificador.registrarObservador(new EmailObserver());
+notificador.registrarObservador(new SMSObserver());
+notificador.notificar(cita);
+
+DB.guardarCita(cita);
